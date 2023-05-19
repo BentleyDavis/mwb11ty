cf99881b82e27cff79b6b859ac918e71c723b5ab
Bentley Davis <Bentley@BentleyDavis.com>
2023-05-18 20:27:10 -0500

    change default page layout to config

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,33 @@ const wikiTransforms = require('./utils/wikiTransforms.js');
 const inputDir = '../massive-wiki/';
 module.exports = function (eleventyConfig) {
 
-    // Links and images
+    // set the default layout 
+    eleventyConfig.addGlobalData("layout", "page.html");
+
+    // wiki Links and images
     eleventyConfig.addTransform("wikiTransforms", wikiTransforms);
+
+    // const inspect = require("util").inspect;
+    // eleventyConfig.addFilter("debug", (content) => `<pre>${inspect(content)}</pre>`);
+
+    // //rewrite readme to index.html
+    // eleventyConfig.addTransform("readme2Index", async function (content) {
+    //     if (this.page.outputPath === 'output/README/index.html') {
+    //         this.page.outputPath = 'output/index.html';
+    //         this.page.fileSlug = '';
+    //         this.page.filePathStem = '/';
+    //         console.log(this.page);
+    //     }
+    //     return content; // no change done.
+    // });
+
+    // //rewrite readme to index.html
+    // eleventyConfig.addUrlTransform(({ url }) => {
+    //     if (url === "/README/") {
+    //         console.log(url);
+    //         return "/README2/";
+    //     }
+    // });
 
     // copy static files
     eleventyConfig.addPassthroughCopy({ "./static/": "/" });
