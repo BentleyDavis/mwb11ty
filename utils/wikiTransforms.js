e9a3cbb08cc17ab3946a9ee569c7a01188836942
Bentley Davis <Bentley@BentleyDavis.com>
2023-05-20 12:39:43 -0500

    wiki images improvement but still edge cases

--- utils/wikiTransforms.js
+++ utils/wikiTransforms.js
@@ -1,5 +1,7 @@
-
+const { parse } = require('path');
 const obsidianImageRegex = /(!?)\[\[([^\]]+)\]\]/g;
+
+//TODO: currently ignores relative links and searches for the file in the index
 
 function wikiTransforms(content, filesIndex) {
     const matches = content.matchAll(obsidianImageRegex);
@@ -8,8 +10,9 @@ function wikiTransforms(content, filesIndex) {
 
     for (const match of matches) {
         linkContent = match[2].split('|');
+        linkFileData = parse(linkContent[0]);
         const labl = linkContent[1] || linkContent[0];
-        let foundFileData = filesIndex[linkContent[0]]?.[0];
+        let foundFileData = filesIndex[linkFileData.name]?.[0];
         let url = "";
         if (foundFileData) {
             url = "/" +
@@ -24,10 +27,9 @@ function wikiTransforms(content, filesIndex) {
 
         if (match[1] === "!") {
             // Is an attachment / image
-            result = result.replace(match[0], `<img src="${url}" alt="${labl}" />`);
+            result = result.replace(match[0], `<img src="${url + foundFileData.nameData.ext}" alt="${labl}" />`);
         } else {
             // Is a link
-            console.log(match[2], url);
             result = result.replace(match[0], `<a href="${url}">${labl}</a>`);
         }
     }
